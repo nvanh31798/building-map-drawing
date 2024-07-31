5de59cd31442895326f3f219dfdecb8ba096935b
Ngo Anh <nvanh31798@gmail.com>
2024-07-31 22:28:36 +0700

    add eslint configuration

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,10 +1,12 @@
-
 import "./App.css";
+import { NavBar } from "./shared-component/NavBar";
 
 function App() {
-  return <div className="App">
-    Hello World!
-  </div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
