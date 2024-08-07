80c05b255c3b2199dc2be179be7d6878872ba449
Ngo Anh <nvanh31798@gmail.com>
2024-08-07 22:56:42 +0700

    add class change for NavBar

--- client/my-app/src/shared-component/NavBar.tsx
+++ client/my-app/src/shared-component/NavBar.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 
 export const NavBar = () => {
   return (
-    <div>
+    <div className="w-full border">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
