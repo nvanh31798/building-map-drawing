c33977554397105c7f0dee973bfaee08565d6028
Ngo Anh <nvanh31798@gmail.com>
2024-08-11 23:01:20 +0700

    as an end user, i am able to see the svg img on dashboard

--- client/my-app/src/components/DrawingMapContainer/DrawingMapContainer.tsx
+++ client/my-app/src/components/DrawingMapContainer/DrawingMapContainer.tsx
@@ -1,38 +1,41 @@
 import { Layer, Stage } from "react-konva";
 import { useAppDispatch, useAppSelector } from "../../core/store/hooks";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { fetchMaterialThunk } from "../../core/thunks/fetchMaterialThunk";
-import useImage from "use-image";
 import { KonvaImage } from "../KonvaImage/KonvaImage";
-import Konva from "konva";
 
 const DrawingMapContainer = () => {
   const materials = useAppSelector((state) => state.drawing.materials);
-  const layerRef = useRef<Konva.Stage>(null);
+  const [layers, setLayers] = useState<JSX.Element[]>([]);
 
   const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    dispatch(fetchMaterialThunk());
+  }, []);
+
+  useEffect(() => {
+    if (!materials || !materials.length) {
+      return;
+    }
+
+    const newLayers = materials.map((material) => (
+      <Layer key={material.id}>
+        <KonvaImage key={material.id} url={material.resourceURL} />
+      </Layer>
+    ));
+    setLayers((prev) => {
+      return [...prev, ...newLayers];
+    });
+  }, [materials]);
+
   return (
     <Stage
-      className="m-5 p-5 border-2"
+      className="m-5 p-5 border-2 bg-red-200"
       width={window.innerWidth}
       height={window.innerHeight}
-      ref={layerRef}
     >
-      <Layer>
-        {materials.length > 0 &&
-          materials.map((material) => {
-            if (!material.resourceURL) {
-              return <h1>Faild to load</h1>;
-            }
-            <KonvaImage key={material.id} url={material.resourceURL} />;
-          })}
-        <KonvaImage
-          url={
-            "https://upload.wikimedia.org/wikipedia/commons/8/8c/SVG_logo_h.svg"
-          }
-        />
-      </Layer>
+      {layers}
     </Stage>
   );
 };
