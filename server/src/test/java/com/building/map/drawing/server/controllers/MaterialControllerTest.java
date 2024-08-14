e5c7b1cc7fa9548c998cf9300ef9e1acec4a787c
Ngo Anh <nvanh31798@gmail.com>
2024-08-14 21:33:26 +0700

    remove redundant code

--- server/src/test/java/com/building/map/drawing/server/controllers/MaterialControllerTest.java
+++ server/src/test/java/com/building/map/drawing/server/controllers/MaterialControllerTest.java
@@ -11,9 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.is;
