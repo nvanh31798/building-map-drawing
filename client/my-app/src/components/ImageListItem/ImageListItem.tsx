97015df53a921acd557a2852a34c3e7c9ec306da
Ngo Anh <nvanh31798@gmail.com>
2024-08-25 23:29:30 +0700

    add image list item

--- client/my-app/src/components/ImageListItem/ImageListItem.tsx
+++ client/my-app/src/components/ImageListItem/ImageListItem.tsx
@@ -6,6 +6,16 @@ interface ImageListItemProps {
 }
 
 export const ImageListItem = ({ image }: ImageListItemProps) => {
-
-  return <div>{image.name}</div>;
+  return (
+    <div className="flex items-center mt-5 p-3 shadow-lg gap-5">
+      <img
+        className="shadow-md"
+        width={50}
+        height={50}
+        src={image.url}
+        alt={image.name}
+      />
+      <p className="text-sm truncate">{image.name}</p>
+    </div>
+  );
 };
