971fdf02f7e7e9fdbb31919372b3d23a3aada755
Ngo Anh <nvanh31798@gmail.com>
2024-09-04 22:51:41 +0700

    update scaling

--- client/my-app/src/components/DrawingMapContainer/DrawingMapContainer.tsx
+++ client/my-app/src/components/DrawingMapContainer/DrawingMapContainer.tsx
@@ -10,7 +10,15 @@ const DrawingMapContainer = () => {
   const imageFiles = useAppSelector((state) => state.image.images);
 
   const [items, setItem] = useState<JSX.Element[]>([]);
+  const [stageScale, setStageScale] = useState(1);
   const stageRef = useContext(StageRefContext);
+
+  const handleOnWheel = (evt: WheelEvent) => {
+    const isWheelUp = evt.deltaY > 0;
+    const scaleValue = isWheelUp ? 0.1 : -0.1;
+
+    setStageScale((prevScale) => prevScale + scaleValue);
+  };
 
   useEffect(() => {
     if (!imageFiles || !imageFiles.length) {
@@ -44,7 +52,14 @@ const DrawingMapContainer = () => {
   }
 
   return (
-    <Stage className="shadow-lg" width={800} height={520} ref={stageRef}>
+    <Stage
+      onWheel={(evt) => handleOnWheel(evt.evt)}
+      scale={{ x: stageScale, y: stageScale }}
+      className="shadow-lg"
+      width={800}
+      height={520}
+      ref={stageRef}
+    >
       <Layer>{items}</Layer>
     </Stage>
   );
